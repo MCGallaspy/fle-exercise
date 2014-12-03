07323c640b0c9cd9a95ef9856f739d4f7f4c3f8d
Michael Gallaspy <gallaspy.michael@gmail.com>
2014-12-03 14:24:14 -0800

    Set url conf, add skeleton templates & views

--- starter/views.py
+++ starter/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def landing_page(request):
-    return render(request, "starter/landing_page.html")
+  return render(request, "starter/movie_list.html")
+
+def movie_detail(request, movie_id):
+  return render(request, "starter/movie_detail.html", {"movie_id": movie_id})
