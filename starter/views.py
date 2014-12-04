38efad4c1d6dba162433ec17071e0089c15ade33
Michael Gallaspy <gallaspy.michael@gmail.com>
2014-12-03 16:12:53 -0800

    Add basic layout for movie list, next style it

--- starter/views.py
+++ starter/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from starter.models import Movie
 
 def landing_page(request):
-  return render(request, "starter/movie_list.html")
+  movies = Movie.objects.all()
+  return render(request, "starter/movie_list.html", {"movies": movies})
 
 def movie_detail(request, movie_id):
   return render(request, "starter/movie_detail.html", {"movie_id": movie_id})
